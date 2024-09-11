64275ff413e443ae66e31ee36a53ab092e35a704
Zaqueu Pereira <zaqueu96@hotmail.com>
2024-09-11 00:06:04 -0300

    feat: created unit test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,104 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as sinon from 'sinon';
+import * as capivaraRunnerExt from '../main/extension'
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('CapivaraRunner Extension Test Suite', () => {
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    let createOutputChannelStub: sinon.SinonStub;
+    let showInformationMessageSinon: sinon.SinonStub;    
+    let showErrorMessageSinon: sinon.SinonStub;
+
+    vscode.window.showInformationMessage('Start all tests.');
+
+    suiteSetup(async () => {
+        await vscode.extensions.getExtension('capivararunner')?.activate();
+    });
+
+    setup(() => {
+        showInformationMessageSinon = sinon.stub(vscode.window, 'showInformationMessage');
+        showErrorMessageSinon = sinon.stub(vscode.window, 'showErrorMessage');
+        //createOutputChannelStub = sinon.stub(vscode.window, 'createOutputChannel');
+    });
+
+    teardown(() => {
+        sinon.restore();
+    });
+
+    suite('And validation load configuration', () => {
+
+        test('Then should not load configuration when capivara.config.json does not exist', async () => {			
+            await vscode.commands.executeCommand('extension.refresh');
+            assert.equal(showErrorMessageSinon.args[0][0],'Config file (capivara.config.json) not found');
+        });
+
+        test('Then should load configuration when capivara.config.json exists', async () => {
+            const configPath = path.join(vscode.workspace.rootPath || '', 'capivara.config.json');
+
+            // Cria o arquivo de configuração
+            fs.writeFileSync(configPath, JSON.stringify({
+                services: [
+                    {
+                        name: "Service1",
+                        workingDirectory: ".",
+                        command: "npm start",
+                        dependsOn: []
+                    }
+                ]
+            }));
+
+            await vscode.commands.executeCommand('extension.refresh');
+
+			assert.equal(showInformationMessageSinon.args[0][0],'Reloaded configuration!');
+     
+            fs.unlinkSync(configPath);
+        });
+
+    });
+
+    suite('And validate commands are registered', () => {
+		let commanndsListRegistered:string[] = []; 
+
+		setup(async () =>{
+			commanndsListRegistered = await vscode.commands.getCommands(true);
+		});
+
+        test('Then should command \'startAllServices\' should be registered',  () => {
+            assert.ok(
+                commanndsListRegistered.includes('extension.startAllServices'),
+                'Command startAllServices was not registered.'
+            );
+        });
+
+		test('Then should command \'stopAllServices\' should be registered', () => {
+            assert.ok(
+                commanndsListRegistered.includes('extension.stopAllServices'),
+                'Command stopAllServices was not registered.'
+            );
+        });
+
+		test('Then should command \'stopService\' should be registered', () => {
+            assert.ok(
+                commanndsListRegistered.includes('extension.stopService'),
+                'Command stopService was not registered.'
+            );
+        });
+
+		test('Then should command \'startService\' should be registered', () => {
+            assert.ok(
+                commanndsListRegistered.includes('extension.startService'),
+                'Command startService was not registered.'
+            );
+        });
+
+		test('Then should command \'refresh\' should be registered', () => {
+            assert.ok(
+                commanndsListRegistered.includes('extension.refresh'),
+                'Command refresh was not registered.'
+            );
+        });
+
+    });
 });
